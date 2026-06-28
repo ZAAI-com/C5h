b97474aa548aedfbc390a40c67283358c7619847
Manuel <m@manuelgruber.com>
2026-06-29 00:03:02 +0200

    Bump app version to 1.2.0 and build number to 3 for next release

--- Packages/C5hCore/Sources/C5hCore/Providers/CodexAppServerClient.swift
+++ Packages/C5hCore/Sources/C5hCore/Providers/CodexAppServerClient.swift
@@ -28,7 +28,7 @@ public struct CodexAppServerClient: Sendable {
         timeoutSeconds: TimeInterval = 15,
         initializeTimeoutSeconds: TimeInterval = 30,
         clientName: String = "C5h",
-        clientVersion: String = "1.1.0"
+        clientVersion: String = "1.2.0"
     ) {
         self.executableURL = executableURL
         self.environment = environment
